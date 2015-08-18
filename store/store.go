f7af46afb5a59d68a56926d0ccea20ad34e74f82
Osiloke Emoekpere <osi@progwebtech.com>
2015-08-18 16:58:50 +0100

    update

--- store/store.go
+++ store/store.go
@@ -1,46 +1,58 @@
 package store
 
 import (
+	"errors"
+	"github.com/franela/goreq"
 	"github.com/mgutz/logxi/v1"
 	. "github.com/osiloke/gostore"
-	"github.com/bndr/gopencils"
-	"github.com/dustin/gojson"
-	"errors"
 	"net/url"
 )
 
 var logger = log.New("gostore.dostow")
 
-type ServerError struct{
+type ServerError struct {
 	code int
-	msg string
+	msg  string
 }
 
-func (e ServerError) Error() string{
+func (e ServerError) Error() string {
 	return e.msg
 }
 
-func newServerError(code int, msg string) ServerError{
+func newServerError(code int, msg string) ServerError {
 	return ServerError{code, msg}
-}
-
-func NewStore(apiUrl, accessKey string) Dostow {
-	s := Dostow{gopencils.Api(apiUrl+"/store"), accessKey}
-	return s
-}
-
-type Dostow struct {
-	api *gopencils.Resource
-	key string
 }
 
 type DostowRows struct {
 }
 
-func (s Dostow) req(store string) *gopencils.Resource{
-	resource := s.api.Res(store)
-	resource.SetHeader("X-DOSTOW-GROUP-ACCESS-KEY", s.key)
-	return resource
+func handleError(err error) error {
+	if e, ok := err.(*url.Error); ok {
+		//e.Err == *net.OpError
+		if serr, ok := err.(*goreq.Error); ok {
+			if serr.Timeout() {
+				return newServerError(500, "Server timeout")
+			}
+			return newServerError(500, err.Error())
+		}
+		if e.Err.Error() == "no such host" {
+			return newServerError(500, "Server Unreachable")
+		}
+		return newServerError(500, "Server Unreachable")
+	}
+	return newServerError(500, "Unable to complete action")
+}
+
+func handleResponse(res *goreq.Response, dst interface{}) error {
+	switch res.StatusCode {
+	case 200:
+		res.Body.FromJsonTo(&dst)
+		return nil
+	case 500, 400, 401:
+		return newServerError(res.StatusCode, "Unable to perform action due to server error")
+	default:
+	}
+	return errors.New("Cannot perfrom action")
 }
 
 func (s DostowRows) Next(dst interface{}) (bool, error) {
@@ -48,7 +60,13 @@ func (s DostowRows) Next(dst interface{}) (bool, error) {
 	return false, nil
 }
 
-func (s DostowRows) Close(){
+func (s DostowRows) Close() {
+}
+
+type Dostow struct {
+	url   string
+	key   string
+	debug bool
 }
 
 func (s Dostow) CreateDatabase() (err error) {
@@ -56,11 +74,43 @@ func (s Dostow) CreateDatabase() (err error) {
 }
 
 func (s Dostow) GetStore() interface{} {
-	return s.api
+	return map[string]string{"url": s.url, "key": s.key}
 }
 
 func (s Dostow) CreateTable(store string, sample interface{}) (err error) {
 	return nil
+}
+func (s Dostow) req(path string) *goreq.Request {
+	req := goreq.Request{
+		Uri:         s.url + "/store/" + path,
+		Accept:      "application/json",
+		ContentType: "application/json",
+		Compression: goreq.Gzip(),
+		ShowDebug:   s.debug,
+	}
+	req.AddHeader("X-DOSTOW-GROUP-ACCESS-KEY", s.key)
+	return &req
+}
+func (s Dostow) get(path string) *goreq.Request {
+	return s.req(path)
+}
+
+func (s Dostow) post(path string) *goreq.Request {
+	req := s.req(path)
+	req.Method = "POST"
+	return req
+}
+
+func (s Dostow) delete(path string) *goreq.Request {
+	req := s.req(path)
+	req.Method = "DELETE"
+	return req
+}
+
+func (s Dostow) put(path string) *goreq.Request {
+	req := s.req(path)
+	req.Method = "PUT"
+	return req
 }
 
 func (s Dostow) All(count int, skip int, store string) (rrows ObjectRows, err error) {
@@ -80,15 +130,15 @@ func (s Dostow) Before(id string, count int, skip int, store string) (rows Objec
 	return nil, errors.New("not implemented")
 }
 
-func (s Dostow) FilterBefore(id string, filter map[string]interface{}, count int, skip int, store string) (rows ObjectRows, err error){
+func (s Dostow) FilterBefore(id string, filter map[string]interface{}, count int, skip int, store string) (rows ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
-func (s Dostow) FilterBeforeCount(id string, filter map[string]interface{}, count int, skip int, store string) (int64, error){
+func (s Dostow) FilterBeforeCount(id string, filter map[string]interface{}, count int, skip int, store string) (int64, error) {
 	return 0, errors.New("not implemented")
 }
 
-func (s Dostow) FilterSince(id string, filter map[string]interface{}, count int, skip int, store string) (rows ObjectRows, err error){
+func (s Dostow) FilterSince(id string, filter map[string]interface{}, count int, skip int, store string) (rows ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
@@ -104,27 +154,26 @@ func (s Dostow) Get(id, store string, dst interface{}) (err error) {
 
 func (s Dostow) Save(store string, src interface{}) (key string, err error) {
 	var msg map[string]interface{}
-	r, err := s.req(store).Post(src)
+	req := s.post(store)
+	req.Body = src
+	res, err := req.Do()
 	if err != nil {
-//		log.Info("err", "err", err)
-		if e, ok := err.(*url.Error); ok{
-			//e.Err == *net.OpError
-			if e.Err.Error() == "no such host"{
-				return "", newServerError(500, "Server Unreachable")
-			}
-			return "", newServerError(500, "Server Unreachable")
-		}
-		return "", newServerError(500, "Unable to complete taction")
-
+		log.Error("Error:", "data", src, "err", err)
+		return "", handleError(err)
 	}
-
-	decoder := json.NewDecoder(r.Raw.Body)
-	decoder.Decode(&msg)
-	switch(r.Raw.StatusCode){
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	switch res.StatusCode {
 	case 200:
+		res.Body.FromJsonTo(&msg)
 		return msg["id"].(string), nil
-	case 500,400,401:
-		return "", newServerError(r.Raw.StatusCode, "Unable to perform action due to server error")
+	case 404:
+		body, _ := res.Body.ToString()
+		return "", newServerError(res.StatusCode, body)
+	case 500, 400, 401:
+		body, _ := res.Body.ToString()
+		return "", newServerError(res.StatusCode, body)
 	default:
 		return "", errors.New("Cannot perfrom action")
 	}
@@ -147,7 +196,13 @@ func (s Dostow) GetByField(name, val, store string, dst interface{}) (err error)
 }
 
 func (s Dostow) FilterGet(filter map[string]interface{}, store string, dst interface{}) (err error) {
-	return errors.New("not implemented")
+	req := s.get(store)
+	req.QueryString = filter
+	res, err := req.Do()
+	if err != nil {
+		return handleError(err)
+	}
+	return handleResponse(res, dst)
 }
 
 func (s Dostow) FilterGetAll(filter map[string]interface{}, count int, skip int, store string) (rrows ObjectRows, err error) {
@@ -167,4 +222,10 @@ func (s Dostow) GetByFieldsByField(name, val, store string, fields []string, dst
 }
 
 func (s Dostow) Close() {
+
+}
+
+func NewStore(apiUrl, accessKey string) Dostow {
+	s := Dostow{url: apiUrl, key: accessKey, debug: true}
+	return s
 }
