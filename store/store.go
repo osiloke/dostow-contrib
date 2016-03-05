b7794905205b8fa58c82fb1a2dbf5283c1e9717f
Osiloke Emoekpere <osi@progwebtech.com>
2016-03-05 07:49:46 +0100

    add some dostow specific methods and fix bugs

--- store/store.go
+++ store/store.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gosexy/to"
 	"github.com/mgutz/logxi/v1"
 	"github.com/mitchellh/mapstructure"
-	. "github.com/osiloke/gostore"
+	"github.com/osiloke/gostore"
 	"strings"
 	// "github.com/smallnest/goreq"
 	"github.com/ddliu/go-httpclient"
@@ -32,6 +32,9 @@ const (
 
 var rl = rate.New(50, time.Second)
 var logger = log.New("gostore.dostow")
+
+var ErrNotFound = gostore.ErrNotFound
+var ErrNotImplemented = gostore.ErrNotImplemented
 
 type ServerError struct {
 	code int
@@ -60,7 +63,7 @@ func newServerError(code int, msg string) ServerError {
 }
 
 type DostowRows struct {
-	data []interface{}
+	data map[string]interface{}
 }
 
 func (s DostowRows) LastError() error {
@@ -70,6 +73,7 @@ func (s DostowRows) Next(dst interface{}) (bool, error) {
 
 	return false, nil
 }
+func (s DostowRows) Raw() map[string]interface{} { return s.data }
 
 func (s DostowRows) Close() {
 }
@@ -98,11 +102,11 @@ func (s Dostow) CreateTable(store string, sample interface{}) (err error) {
 	return nil
 }
 
-func (s Dostow) All(count int, skip int, store string) (rrows ObjectRows, err error) {
+func (s Dostow) All(count int, skip int, store string) (rrows gostore.ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
-func (s Dostow) AllCursor(store string) (ObjectRows, error) {
+func (s Dostow) AllCursor(store string) (gostore.ObjectRows, error) {
 	return nil, errors.New("not implemented")
 }
 
@@ -111,39 +115,32 @@ func (s Dostow) AllCursor(store string) (ObjectRows, error) {
 //r.db('worksmart').table('store').orderBy({'index': r.desc('id')}).filter(r.row('schemas')
 // .eq('osiloke_tsaboin_silverbird').and(r.row('id').lt('55b54e93f112a16514000057')))
 // .pluck('schemas', 'id','tid', 'timestamp', 'created_at').limit(100)
-func (s Dostow) Before(id string, count int, skip int, store string) (rows ObjectRows, err error) {
+func (s Dostow) Before(id string, count int, skip int, store string) (rows gostore.ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
-func (s Dostow) FilterBefore(id string, filter map[string]interface{}, count int, skip int, store string, opts ObjectStoreOptions) (rows ObjectRows, err error) {
+func (s Dostow) FilterBefore(id string, filter map[string]interface{}, count int, skip int, store string, opts gostore.ObjectStoreOptions) (rows gostore.ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
-func (s Dostow) FilterBeforeCount(id string, filter map[string]interface{}, count int, skip int, store string, opts ObjectStoreOptions) (int64, error) {
+func (s Dostow) FilterBeforeCount(id string, filter map[string]interface{}, count int, skip int, store string, opts gostore.ObjectStoreOptions) (int64, error) {
 	return 0, errors.New("not implemented")
 }
 
-func (s Dostow) FilterSince(id string, filter map[string]interface{}, count int, skip int, store string, opts ObjectStoreOptions) (rows ObjectRows, err error) {
+func (s Dostow) FilterSince(id string, filter map[string]interface{}, count int, skip int, store string, opts gostore.ObjectStoreOptions) (rows gostore.ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
 //This will retrieve all new rows that were created since the row with id was created
 // [1, 2, 3, 4], since 2 will return [1]
-func (s Dostow) Since(id string, count, skip int, store string) (rrows ObjectRows, err error) {
+func (s Dostow) Since(id string, count, skip int, store string) (rrows gostore.ObjectRows, err error) {
 	return nil, errors.New("not implemented")
 }
 
-func (s *Dostow) post(url string, data string) (*httpclient.Response, error) {
-	// Post with files should be sent as multipart.
-	headers := make(map[string]string)
-	headers["X-DOSTOW-GROUP-ACCESS-KEY"] = s.key
-	headers["Content-Type"] = "application/json"
-	body := strings.NewReader(data)
-	// httpclient.NewHttpClient(defaults)
+func (s *Dostow) getClient() *httpclient.HttpClient {
 	rl.Wait()
-	return httpclient.Begin().Do("POST", url, headers, body)
+	return httpclient.Begin().WithHeader("X-DOSTOW-GROUP-ACCESS-KEY", s.key).WithHeader("Content-Type", "application/json")
 }
-
 func (s *Dostow) get(url string, params map[string]string) (*httpclient.Response, error) {
 	// Post with files should be sent as multipart.
 	rl.Wait()
@@ -162,7 +159,35 @@ func (s *Dostow) put(url string, data string) (*httpclient.Response, error) {
 }
 
 func (s Dostow) Get(id, store string, dst interface{}) (err error) {
-	return errors.New("not implemented")
+	resp, err := s.get(s.url+"/store/"+store+"/"+id, nil)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	switch resp.StatusCode {
+	case 200:
+		if err := json.Unmarshal(bodyBytes, &dst); err != nil {
+			return err
+		}
+	case 500, 400, 401:
+		return newServerError(resp.StatusCode, "Unable to perform action due to server error")
+	default:
+		return errors.New("Cannot perfrom action")
+	}
+	return
+}
+
+func (s *Dostow) post(url string, data string) (*httpclient.Response, error) {
+	// Post with files should be sent as multipart.
+	headers := make(map[string]string)
+	headers["X-DOSTOW-GROUP-ACCESS-KEY"] = s.key
+	headers["Content-Type"] = "application/json"
+	body := strings.NewReader(data)
+	// httpclient.NewHttpClient(defaults)
+	rl.Wait()
+	return httpclient.Begin().Do("POST", url, headers, body)
 }
 
 func (s Dostow) Save(store string, src interface{}) (key string, err error) {
@@ -253,19 +278,15 @@ func (s Dostow) Replace(id string, store string, src interface{}) (err error) {
 	}
 	defer resp.Body.Close()
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	switch resp.StatusCode {
-	case 200:
+	if resp.StatusCode == 200 {
 		if err := json.Unmarshal(bodyBytes, &msg); err == nil {
 			return nil
 		} else {
 			return err
 		}
-	case 404:
-		return newServerError(resp.StatusCode, string(bodyBytes))
-	case 500, 400, 401:
-		return newServerError(resp.StatusCode, string(bodyBytes))
-	default:
-		return errors.New("Cannot perform action")
+	} else {
+		return handleErrorResponse(resp.StatusCode, bodyBytes)
+
 	}
 }
 
@@ -282,7 +303,7 @@ func (s Dostow) GetByField(name, val, store string, dst interface{}) (err error)
 }
 
 //FIlterGet gets one item from a store based on some filter
-func (s Dostow) FilterGet(filter map[string]interface{}, store string, dst interface{}, opts ObjectStoreOptions) (err error) {
+func (s Dostow) FilterGet(filter map[string]interface{}, store string, dst interface{}, opts gostore.ObjectStoreOptions) (err error) {
 	if filter == nil {
 		filter = map[string]interface{}{}
 	}
@@ -331,7 +352,11 @@ func (s Dostow) FilterGet(filter map[string]interface{}, store string, dst inter
 	return
 }
 
-func (s Dostow) FilterGetAll(filter map[string]interface{}, count int, skip int, store string, opts ObjectStoreOptions) (rrows ObjectRows, err error) {
+func (s Dostow) AllWithinRange(filter map[string]interface{}, count int, skip int, store string, opts gostore.ObjectStoreOptions) (gostore.ObjectRows, error) {
+	return nil, ErrNotImplemented
+}
+
+func (s Dostow) FilterGetAll(filter map[string]interface{}, count int, skip int, store string, opts gostore.ObjectStoreOptions) (rrows gostore.ObjectRows, err error) {
 	params := map[string]string{}
 	for k, v := range filter {
 		params[k] = to.String(v)
@@ -356,7 +381,7 @@ func (s Dostow) FilterGetAll(filter map[string]interface{}, count int, skip int,
 	}
 	defer resp.Body.Close()
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	var dst []interface{}
+	var dst map[string]interface{}
 	switch resp.StatusCode {
 	case 200:
 		if err := json.Unmarshal(bodyBytes, &dst); err != nil {
@@ -371,19 +396,19 @@ func (s Dostow) FilterGetAll(filter map[string]interface{}, count int, skip int,
 	return nil, errors.New("Not found")
 }
 
-func (s Dostow) FilterDelete(filter map[string]interface{}, store string, opts ObjectStoreOptions) (err error) {
+func (s Dostow) FilterDelete(filter map[string]interface{}, store string, opts gostore.ObjectStoreOptions) (err error) {
 	return errors.New("not implemented")
 }
 
-func (s Dostow) FilterUpdate(filter map[string]interface{}, src interface{}, store string, opts ObjectStoreOptions) (err error) {
+func (s Dostow) FilterUpdate(filter map[string]interface{}, src interface{}, store string, opts gostore.ObjectStoreOptions) (err error) {
 	return errors.New("Not Implemented")
 }
 
-func (s Dostow) FilterReplace(filter map[string]interface{}, src interface{}, store string, opts ObjectStoreOptions) (err error) {
+func (s Dostow) FilterReplace(filter map[string]interface{}, src interface{}, store string, opts gostore.ObjectStoreOptions) (err error) {
 	return errors.New("Not Implemented")
 }
 
-func (s Dostow) FilterCount(filter map[string]interface{}, store string, opts ObjectStoreOptions) (int64, error) {
+func (s Dostow) FilterCount(filter map[string]interface{}, store string, opts gostore.ObjectStoreOptions) (int64, error) {
 	return 0, errors.New("not implemented")
 }
 
@@ -448,8 +473,263 @@ func streamingUploadFile(id, field, path, store string, w *io.PipeWriter, file i
 // 	// return
 // }
 
+//  __ __  ____  ____ _____     __  __   ____   __  _   ____   ____  ____  __  __  ____  __  _  _____
+// |  |  |(_ (_`| ===|| () )   |  \/  | / () \ |  \| | / () \ / (_,`| ===||  \/  || ===||  \| ||_   _|
+//  \___/.__)__)|____||_|\_\   |_|\/|_|/__/\__\|_|\__|/__/\__\\____)|____||_|\/|_||____||_|\__|  |_|
+
+type RegisterForm struct {
+	Username  string `json:"Username,omitempty"`
+	Email     string `json:"Email,omitempty"`
+	Password  []byte `json:"Password,omitempty"`
+	Password2 []byte `json:"Password2,omitempty"`
+}
+
+func (s Dostow) Register(email, username, password string) (user map[string]interface{}, err error) {
+	data, err := json.Marshal(&RegisterForm{Email: email, Username: username, Password: []byte(password), Password2: []byte(password)})
+	if err != nil {
+		return nil, err
+	}
+	resp, err := s.post(s.url+"/auth/register", string(data))
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		err = json.Unmarshal(bodyBytes, &user)
+		return
+	} else {
+		logger.Warn("error while registring", "code", resp.StatusCode, "body", string(bodyBytes))
+		var errData map[string]interface{}
+		if err := json.Unmarshal(bodyBytes, &errData); err == nil {
+			if msg, ok := errData["msg"].(string); ok {
+				if msg == "auth: User already exists" {
+					return nil, newServerError(400, "User already exists")
+				} else {
+					return nil, newServerError(400, errData["msg"].(string))
+				}
+			}
+		}
+	}
+	return nil, handleErrorResponse(resp.StatusCode, bodyBytes)
+}
+func (s Dostow) PasswordReset(email string) (ret map[string]interface{}, err error) {
+	form := `{"email":"` + email + `"}`
+	resp, err := s.post(s.url+"/auth/reset_password", form)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		if err = json.Unmarshal(bodyBytes, &ret); err == nil {
+			return
+		} else {
+			return nil, err
+		}
+	} else {
+		var errData map[string]interface{}
+		if err = json.Unmarshal(bodyBytes, &errData); err == nil {
+			if msg, ok := errData["msg"].(string); ok {
+				return nil, newServerError(resp.StatusCode, msg)
+			}
+		} else {
+			return nil, newServerError(resp.StatusCode, err.Error())
+		}
+	}
+	return nil, handleErrorResponse(resp.StatusCode, bodyBytes)
+}
+func (s Dostow) SetPassword(token, password string) (ret map[string]interface{}, err error) {
+	form := `{"token":"` + token + `", "password":"` + password + `", "password2":"` + password + `"}`
+	resp, err := s.post(s.url+"/auth/set_password", form)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		if err = json.Unmarshal(bodyBytes, &ret); err == nil {
+			return
+		} else {
+			return nil, err
+		}
+	} else {
+		var errData map[string]interface{}
+		if err = json.Unmarshal(bodyBytes, &errData); err == nil {
+			if msg, ok := errData["msg"].(string); ok {
+				return nil, newServerError(resp.StatusCode, msg)
+			}
+		} else {
+			return nil, newServerError(resp.StatusCode, err.Error())
+		}
+	}
+	return nil, handleErrorResponse(resp.StatusCode, bodyBytes)
+}
+func (s Dostow) SignIn(username, email, password string) (user map[string]interface{}, err error) {
+	form := `{`
+	if username != "" {
+		form = form + `"username":"` + username + `"`
+	} else if email != "" {
+		form = form + `"email":"` + email + `"`
+	}
+	form = form + `,"password":"` + password + `"}`
+	resp, err := s.post(s.url+"/auth/sign_in", form)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		if err = json.Unmarshal(bodyBytes, &user); err == nil {
+			return
+		} else {
+			return nil, err
+		}
+	} else {
+		var errData map[string]interface{}
+		if err = json.Unmarshal(bodyBytes, &errData); err == nil {
+			if msg, ok := errData["msg"].(string); ok {
+				return nil, newServerError(resp.StatusCode, msg)
+			}
+		} else {
+			return nil, newServerError(resp.StatusCode, err.Error())
+		}
+	}
+	return nil, handleErrorResponse(resp.StatusCode, bodyBytes)
+}
+func (s Dostow) SignOut(token string) (err error) {
+	var user map[string]interface{}
+
+	resp, err := s.getClient().WithHeader("Authorization", "bearer "+token).Do("DELETE", s.url+"/auth/sign_out", nil, nil)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		err = json.Unmarshal(bodyBytes, &user)
+		return
+	} else {
+		var msg map[string]interface{}
+		if err = json.Unmarshal(bodyBytes, &msg); err == nil {
+			return newServerError(resp.StatusCode, msg["msg"].(string))
+		}
+	}
+	return handleErrorResponse(resp.StatusCode, bodyBytes)
+}
+
+func (s Dostow) Me(token string) (data map[string]interface{}, err error) {
+	var user map[string]interface{}
+
+	resp, err := s.getClient().WithHeader("Authorization", "bearer "+token).Get(s.url+"/auth/me", nil)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
+		if err := json.Unmarshal(bodyBytes, &user); err == nil {
+			return user, nil
+		} else {
+			return nil, err
+		}
+	} else {
+		var msg map[string]interface{}
+		if err = json.Unmarshal(bodyBytes, &msg); err == nil {
+			return nil, newServerError(resp.StatusCode, msg["msg"].(string))
+		}
+	}
+	return nil, handleErrorResponse(resp.StatusCode, bodyBytes)
+}
+
+//   .--.  .----. .-.   .-..-..-. .-.   .----..-. .-..----. .----.  .----. .-..-. .-. .---.  .----.
+//  / {} \ | {}  \|  `.'  || ||  `| |   | {_  |  `| || {}  \| {}  }/  {}  \| ||  `| |{_   _}{ {__
+// /  /\  \|     /| |\ /| || || |\  |   | {__ | |\  ||     /| .--' \      /| || |\  |  | |  .-._} }
+// `-'  `-'`----' `-' ` `-'`-'`-' `-'   `----'`-' `-'`----' `-'     `----' `-'`-' `-'  `-'  `----'
+
+type Group struct {
+	CreatedAt  time.Time `json:"created_at"`
+	ID         string    `json:"id"`
+	ModifiedAt time.Time `json:"modified_at"`
+	Name       string    `json:"name"`
+	Owner      struct {
+		Created    time.Time `json:"Created"`
+		Username   string    `json:"Username"`
+		ID         string    `json:"id"`
+		ModifiedAt time.Time `json:"modified_at"`
+	} `json:"owner"`
+}
+
+func (s Dostow) GetGroup(id string, token string) (*Group, error) {
+	resp, err := s.getClient().
+		WithHeader("Authorization", "bearer "+token).
+		Get(s.url+"/group/"+id, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	var dst Group
+	switch resp.StatusCode {
+	case 200:
+		if err := json.Unmarshal(bodyBytes, &dst); err != nil {
+			return nil, err
+		}
+		return &dst, err
+	case 500, 400, 401:
+		return nil, newServerError(resp.StatusCode, "Unable to perform action due to server error")
+	default:
+		return nil, errors.New("Cannot perfrom action")
+	}
+	return nil, errors.New("Not found")
+}
+func (s Dostow) Schemas(token, group string) (gostore.ObjectRows, error) {
+	//TODO: recursively get all schemas
+	resp, err := s.getClient().
+		WithHeader("Authorization", "bearer "+token).
+		WithHeader("X-DOSTOW-GROUP", group).
+		Get(s.url+"/schemas", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	var dst map[string]interface{}
+	switch resp.StatusCode {
+	case 200:
+		if err := json.Unmarshal(bodyBytes, &dst); err != nil {
+			return nil, err
+		}
+		return DostowRows{dst}, err
+	case 500:
+		return nil, newServerError(resp.StatusCode, "Unable to perform action due to server error")
+	case 400, 401:
+		var errMsg map[string]interface{}
+		if err := json.Unmarshal(bodyBytes, &errMsg); err != nil {
+			return nil, err
+		}
+		logger.Info("failed in retrieving schemas", "msg", errMsg)
+		return nil, newServerError(resp.StatusCode, errMsg["msg"].(string))
+	default:
+		return nil, errors.New("Cannot perfrom action")
+	}
+	return nil, errors.New("Not found")
+}
+
 func (s Dostow) Close() {
 
+}
+
+func handleErrorResponse(code int, bodyBytes []byte) error {
+	switch code {
+	case 404:
+		return ErrNotFound
+	case 500, 400, 401:
+		return newServerError(code, string(bodyBytes))
+	default:
+		return errors.New("Cannot perform action")
+	}
 }
 func handleError(err error) error {
 	if err != nil {
