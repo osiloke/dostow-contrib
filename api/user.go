edb6203b2683961c8f2af4b07f2d1a75b494a751
Osiloke Emoekpere <osi@progwebtech.com>
2016-11-18 12:29:15 +0100

    add authentication endpoints to auth service

--- api/user.go
+++ api/user.go
@@ -3,7 +3,7 @@ package api
 import (
 	// "fmt"
 	// "net/http"
-
+	"encoding/json"
 	"github.com/dghubble/sling"
 )
 
@@ -11,8 +11,49 @@ type AuthService struct {
 	sling *sling.Sling
 }
 
+type SignInRequest struct {
+	Email    string `json:"email"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+type RegisterRequest struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type ResetPasswordRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func newAuthService(sling *sling.Sling) *AuthService {
 	return &AuthService{
 		sling: sling.Path("auth/"),
 	}
+}
+
+func (s *AuthService) SignIn(req *SignInRequest) (*json.RawMessage, error) {
+	var result *json.RawMessage = &json.RawMessage{}
+	apiError := new(APIError)
+	_s := s.sling.New().Post("sign_in").BodyJSON(req)
+	_, err := _s.Receive(result, apiError)
+	return result, relevantError(err, apiError)
+}
+
+func (s *AuthService) Register(req *RegisterRequest) (*json.RawMessage, error) {
+	var result *json.RawMessage = &json.RawMessage{}
+	apiError := new(APIError)
+	_s := s.sling.New().Post("register").BodyJSON(req)
+	_, err := _s.Receive(result, apiError)
+	return result, relevantError(err, apiError)
+}
+
+func (s *AuthService) ResetPassword(req *ResetPasswordRequest) (*json.RawMessage, error) {
+	var result *json.RawMessage = &json.RawMessage{}
+	apiError := new(APIError)
+	_s := s.sling.New().Post("reset_password").BodyJSON(req)
+	_, err := _s.Receive(result, apiError)
+	return result, relevantError(err, apiError)
 }
